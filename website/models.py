6d9c25b6101f9ec179fa5ae83e04ed1f4fee3414
garibaldiviolin <garibaldiviolin@gmail.com>
2018-08-31 16:17:57 -0300

    Product model added

--- website/models.py
+++ website/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    '''
+    - The barcode length was chosen considering that usual products come with
+      EAN-8, EAN-13, UPC-A or UPC-E
+    - The price max length was chosen considering that the most expensive
+      product in the marketplace is 999.999,999 (using 3 decimal places).
+      However, this can be changed if needed
+    '''
+
+    barcode = models.CharField(primary_key=True, max_length=20)
+    title = models.TextArea()
+    description = models.TextArea()
+    price = models.DecimalField(max_digits=8, decimal_places=3)
+
+
+# class PurchaseOrder(models.Model):
