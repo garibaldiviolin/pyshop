c405bd7d3ee4d3d6e0e3035411cb006bd1f17798
garibaldiviolin <garibaldiviolin@gmail.com>
2018-09-02 13:12:28 -0300

    index path added to website/urls.py

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import ProductsView
 
 urlpatterns = [
+    path('', ProductsView.as_view(), name='index'),
+    path('index/', ProductsView.as_view(), name='index'),
     path('products/', ProductsView.as_view()),
 ]
