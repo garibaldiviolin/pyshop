ddfdf0ffcc8b86be386b36b612ec2d49938a2305
garibaldiviolin <garibaldiviolin@gmail.com>
2018-09-02 13:12:16 -0300

    index path added to pyshop/urls.py

--- pyshop/urls.py
+++ pyshop/urls.py
@@ -22,7 +22,8 @@ from pyshop import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('website/', include('website.urls')),
+    path('', include('website.urls')),
 ]
 
+# enable permissions to read media folder
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
