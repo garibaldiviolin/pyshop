2f7bc3aff4d6bb1768b3aa4feb1161f7b8b40bb5
garibaldiviolin <garibaldiviolin@gmail.com>
2018-09-02 13:46:10 -0300

    app_name added to website/urls.py

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import ProductsView
 
+app_name = 'website'
+
 urlpatterns = [
     path('', ProductsView.as_view(), name='index'),
     path('index/', ProductsView.as_view(), name='index'),
