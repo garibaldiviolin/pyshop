6b2166fbd3159c68ccb05db4cbf53b19f53555e2
garibaldiviolin <garibaldiviolin@gmail.com>
2018-09-02 13:47:18 -0300

    q input parameter added to qqueryset in website/templates/products.html and website/views.py

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.db.models import Q
 
 from .models import Product
 
@@ -7,4 +8,15 @@ class ProductsView(ListView):
 
     template_name = "products.html"
     context_object_name = 'product_list'
-    queryset = Product.objects.all()
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+
+        q = self.request.GET.get('q', '')
+
+        if len(q) > 0:
+            queryset = queryset.filter(
+                Q(title__icontains=q) |
+                Q(description__icontains=q)
+            )
+        return queryset
