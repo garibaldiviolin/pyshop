3e709ba471a1961cf69278fb02825953a4e068c1
garibaldiviolin <garibaldiviolin@gmail.com>
2018-09-03 13:01:59 -0300

    static files and css added

--- pyshop/urls.py
+++ pyshop/urls.py
@@ -27,3 +27,4 @@ urlpatterns = [
 
 # enable permissions to read media folder
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
