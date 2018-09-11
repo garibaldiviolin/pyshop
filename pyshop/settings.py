075f25a4b7c50176e0f75dd23960291a30600126
garibaldiviolin <garibaldiviolin@gmail.com>
2018-09-10 23:56:11 -0300

    manager app created

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'website',
+    'manager',
 ]
 
 MIDDLEWARE = [
--- website/forms.py
+++ website/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from .models import Product
+
 
 class UserForm(forms.ModelForm):
 
@@ -29,3 +31,6 @@ class SignUpForm(UserCreationForm):
             'username', 'first_name', 'last_name',
             'email', 'password1', 'password2',
         )
+
+
+class ProductForm(Product)
