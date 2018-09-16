a19f724e66dbb9e51f8356a313aeba74e1638b34
garibaldiviolin <garibaldiviolin@gmail.com>
2018-09-15 21:29:00 -0300

    i18n added to website/models.py

--- website/models.py
+++ website/models.py
@@ -2,13 +2,20 @@ from django.conf import settings
 from django.db.models.signals import pre_save
 from django.db import models
 from django.urls import reverse
+from django.utils.translation import ugettext_lazy
 
 from .utils import unique_slug_generator
 
 
 class Category(models.Model):
 
-    description = models.CharField(max_length=50)
+    description = models.CharField(
+        max_length=50, verbose_name=ugettext_lazy('Description')
+    )
+
+    class Meta:
+        verbose_name = ugettext_lazy('Category')
+        verbose_name_plural = ugettext_lazy('Categories')
 
     def __repr__(self):
         return self.description
@@ -22,12 +29,20 @@ class Category(models.Model):
 
 class ProductBase(models.Model):
 
-    barcode = models.CharField(primary_key=True, max_length=20)
-    title = models.TextField()
-    description = models.TextField()
-    image = models.ImageField()
-    price = models.DecimalField(max_digits=8, decimal_places=3)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    barcode = models.CharField(
+        primary_key=True, max_length=20,
+        verbose_name=ugettext_lazy('Barcode')
+    )
+    title = models.TextField(verbose_name=ugettext_lazy('Title'))
+    description = models.TextField(verbose_name=ugettext_lazy('Description'))
+    image = models.ImageField(verbose_name=ugettext_lazy('Image'))
+    price = models.DecimalField(
+        max_digits=8, decimal_places=3, verbose_name=ugettext_lazy('Price')
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE,
+        verbose_name=ugettext_lazy('Category')
+    )
 
     class Meta:
 
@@ -52,7 +67,13 @@ class Product(ProductBase):
       However, this can be changed if needed
     '''
 
-    barcode = models.CharField(primary_key=True, max_length=20)
+    class Meta:
+        verbose_name = ugettext_lazy('Product')
+        verbose_name_plural = ugettext_lazy('Products')
+
+    barcode = models.CharField(
+        primary_key=True, max_length=20, verbose_name=ugettext_lazy('Barcode')
+    )
     slug = models.SlugField(unique=True)
 
     def get_absolute_url(self):
@@ -69,7 +90,13 @@ pre_save.connect(pre_save_product_receiver, sender=Product)
 
 class PaymentMethod(models.Model):
 
-    description = models.CharField(unique=True, max_length=50)
+    description = models.CharField(
+        unique=True, max_length=50, verbose_name=ugettext_lazy('Description')
+    )
+
+    class Meta:
+        verbose_name = ugettext_lazy('PaymentMethod')
+        verbose_name_plural = ugettext_lazy('PaymentMethods')
 
     def __repr__(self):
         return self.description
@@ -83,12 +110,17 @@ class PaymentMethod(models.Model):
 
 class PurchaseOrder(models.Model):
 
-    timestamp = models.DateTimeField()
+    timestamp = models.DateTimeField(verbose_name=ugettext_lazy('Timestamp'))
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
+        verbose_name=ugettext_lazy('User')
     )
-    cart = models.BooleanField()
+    cart = models.BooleanField(verbose_name=ugettext_lazy('Cart'))
+
+    class Meta:
+        verbose_name = ugettext_lazy('PurchaseOrder')
+        verbose_name_plural = ugettext_lazy('PurchaseOrders')
 
     def __repr__(self):
         return str(self.timestamp) + self.user.username
@@ -102,10 +134,22 @@ class PurchaseOrder(models.Model):
 
 class PurchaseItem(ProductBase):
 
-    purchase_order = models.ForeignKey(PurchaseOrder, on_delete=models.CASCADE)
-    quantity = models.DecimalField(max_digits=8, decimal_places=3)
-    total_price = models.DecimalField(max_digits=8, decimal_places=3)
+    purchase_order = models.ForeignKey(
+        PurchaseOrder, on_delete=models.CASCADE,
+        verbose_name=ugettext_lazy('PurchaseOrder')
+    )
+    quantity = models.DecimalField(
+        max_digits=8, decimal_places=3, verbose_name=ugettext_lazy('Quantity')
+    )
+    total_price = models.DecimalField(
+        max_digits=8, decimal_places=3,
+        verbose_name=ugettext_lazy('TotalPrice')
+    )
     slug = None  # this class doesn't use this attribute
+
+    class Meta:
+        verbose_name = ugettext_lazy('PurchaseItem')
+        verbose_name_plural = ugettext_lazy('PurchaseItems')
 
     def __repr__(self):
         return str(self.id)
@@ -119,11 +163,21 @@ class PurchaseItem(ProductBase):
 
 class PurchasePaymentMethod(models.Model):
 
-    purchase_order = models.ForeignKey(PurchaseOrder, on_delete=models.CASCADE)
-    payment_method = models.ForeignKey(
-        PaymentMethod, on_delete=models.PROTECT
+    purchase_order = models.ForeignKey(
+        PurchaseOrder, on_delete=models.CASCADE,
+        verbose_name=ugettext_lazy('PurchaseOrder')
     )
-    value = models.DecimalField(max_digits=8, decimal_places=3)
+    payment_method = models.ForeignKey(
+        PaymentMethod, on_delete=models.PROTECT,
+        verbose_name=ugettext_lazy('PaymentMethod')
+    )
+    value = models.DecimalField(
+        max_digits=8, decimal_places=3, verbose_name=ugettext_lazy('Value')
+    )
+
+    class Meta:
+        verbose_name = ugettext_lazy('PurchasePaymentMethod')
+        verbose_name_plural = ugettext_lazy('PurchasePaymentMethods')
 
     def __repr__(self):
         return str(self.id)
