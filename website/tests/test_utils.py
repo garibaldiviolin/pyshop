db828cbfafff7b1d8b7749474c4544acc94cd5a0
garibaldiviolin <garibaldiviolin@gmail.com>
2018-10-01 17:30:55 -0300

    Add test_utils.py to website app

---
+++ website/tests/test_utils.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+
+from website.models import Category, Product
+from website.utils import unique_slug_generator
+
+
+class UniqueSlugGeneratorTest(TestCase):
+    """ Test case for unique_slug_generator method """
+
+    def setUp(self):
+        self.category = Category.objects.create(
+            description='Category'
+        )
+
+    def test_slug(self):
+        expected_result = 'new-mattress'
+        product = Product(
+            barcode='5901234123457', title='New Mattress',
+            description='Mattress', image='mattress.jpg',
+            price=800.724, category=self.category
+        )
+        slug = unique_slug_generator(product)
+        self.assertEquals(slug, expected_result)
