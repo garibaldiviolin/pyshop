e15abe0963c63a8973c33a69beb4a9e5b0ee5f9b
garibaldiviolin <garibaldiviolin@gmail.com>
2018-10-01 15:56:58 -0300

    Remove unused Userform from website/forms.py

--- website/forms.py
+++ website/forms.py
@@ -3,15 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-class UserForm(forms.ModelForm):
-
-    password = forms.CharField(widget=forms.PasswordInput)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
-
-
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(
         max_length=30, required=False, help_text='Optional.'
