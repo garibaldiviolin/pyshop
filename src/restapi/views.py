8a0152d6e4fc744cb63fb3c380fe2dd711cd0cfd
garibaldiviolin <garibaldiviolin@gmail.com>
2018-10-04 17:40:30 -0300

    Add ProductCreateView to restapi views.py

--- src/restapi/views.py
+++ src/restapi/views.py
@@ -1,7 +1,7 @@
 from rest_framework.generics import CreateAPIView
 
-from website.models import Category
-from .serializers import CategorySerializer
+from website.models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
 
 class CategoryCreateView(CreateAPIView):
@@ -10,3 +10,11 @@ class CategoryCreateView(CreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     lookup_field = 'description'
+
+
+class ProductCreateView(CreateAPIView):
+    """ Create view for Product objects """
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'barcode'
