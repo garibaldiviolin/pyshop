0b13bfdc672af589fc328f74ef033bd359e36b4f
garibaldiviolin <garibaldiviolin@gmail.com>
2018-10-04 11:38:50 -0300

    Add restapi app and django rest framework

--- src/pyshop/settings.py
+++ src/pyshop/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'website',
     'manager',
+    'restapi',
 ]
 
 MIDDLEWARE = [
