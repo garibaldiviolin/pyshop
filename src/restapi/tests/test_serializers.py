5a322758018a6621c80a4ae16de4a4b6320cdfc3
garibaldiviolin <garibaldiviolin@gmail.com>
2018-10-04 23:16:00 -0300

    Update test_serializers.py in restapi app

--- src/restapi/tests/test_serializers.py
+++ src/restapi/tests/test_serializers.py
@@ -61,7 +61,7 @@ class ProductSerializerTest(TestCase):
             barcode='789789789',
             title='Mattress',
             description='Sleep Mattress',
-            image='{}/{}'.format(BASE_DIR, 'download.jpg'),
+            image=self.image,
             price=123.000,
             category=self.category
         )
@@ -107,6 +107,71 @@ class ProductSerializerTest(TestCase):
         self.assertEqual(
             self.product.category.id,
             self.serializer.data['category']
+        )
+
+    def test_blank_barcode(self):
+        """ Test using blank barcode value """
+
+        data = self.json
+        data.update({'barcode': ''})
+        exception_serializer = ProductSerializer(data=data)
+
+        with self.assertRaises(serializers.ValidationError):
+            exception_serializer.is_valid(raise_exception=True)
+        self.assertIn('barcode', exception_serializer.errors.keys())
+        self.assertIn('blank', str(exception_serializer.errors.values()))
+
+    def test_blank_title(self):
+        """ Test using blank title value """
+
+        data = self.json
+        data.update({'title': ''})
+        exception_serializer = ProductSerializer(data=data)
+
+        with self.assertRaises(serializers.ValidationError):
+            exception_serializer.is_valid(raise_exception=True)
+        self.assertIn('title', exception_serializer.errors.keys())
+        self.assertIn('blank', str(exception_serializer.errors.values()))
+
+    def test_blank_description(self):
+        """ Test using blank description value """
+
+        data = self.json
+        data.update({'description': ''})
+        exception_serializer = ProductSerializer(data=data)
+
+        with self.assertRaises(serializers.ValidationError):
+            exception_serializer.is_valid(raise_exception=True)
+        self.assertIn('description', exception_serializer.errors.keys())
+        self.assertIn('blank', str(exception_serializer.errors.values()))
+
+    def test_blank_image(self):
+        """ Test using blank image value """
+
+        data = self.json
+        data.update({'image': ''})
+        exception_serializer = ProductSerializer(data=data)
+
+        with self.assertRaises(serializers.ValidationError):
+            exception_serializer.is_valid(raise_exception=True)
+        self.assertIn('image', exception_serializer.errors.keys())
+        self.assertIn(
+            'The submitted data was not a file',
+            str(exception_serializer.errors.values())
+        )
+
+    def test_blank_price(self):
+        """ Test using blank price value """
+
+        data = self.json
+        data.update({'price': ''})
+        exception_serializer = ProductSerializer(data=data)
+
+        with self.assertRaises(serializers.ValidationError):
+            exception_serializer.is_valid(raise_exception=True)
+        self.assertIn('price', exception_serializer.errors.keys())
+        self.assertIn(
+            'valid number', str(exception_serializer.errors.values())
         )
 
     def test_null_category(self):
