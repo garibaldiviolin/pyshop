d76338bbbccb94e867e08af43f268afbc2b01d72
garibaldiviolin <garibaldiviolin@gmail.com>
2018-10-06 13:06:26 -0300

    Add docstrings to restapi/tests/test_urls.py

--- src/restapi/tests/test_urls.py
+++ src/restapi/tests/test_urls.py
@@ -1,3 +1,5 @@
+""" This module tests restapi app urls """
+
 from django.urls import reverse
 from django.test import TestCase
 
@@ -6,12 +8,15 @@ class RESTAPIURLsTest(TestCase):
     """ Test case for the restapi app urls """
 
     def test_categories_url(self):
+        """ Test restapi:categories url """
         self.assertEqual(reverse('restapi:categories'), '/api/v1/categories/')
 
     def test_products_url(self):
+        """ Test restapi:products url """
         self.assertEqual(reverse('restapi:products'), '/api/v1/products/')
 
     def test_payment_methods_url(self):
+        """ Test restapi:payment-methods url """
         self.assertEqual(
             reverse('restapi:payment-methods'),
             '/api/v1/payment-methods/'
