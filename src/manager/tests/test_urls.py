e1b1eba6dc2134b64fd3345cbe13a138e1be345b
garibaldiviolin <garibaldiviolin@gmail.com>
2018-10-06 19:00:29 -0300

    Add docstrings to manager test_urls.py

--- src/manager/tests/test_urls.py
+++ src/manager/tests/test_urls.py
@@ -6,43 +6,52 @@ class ManagerURLsTest(TestCase):
     """ Test case for the manager app urls """
 
     def test_index_url(self):
+        """ Test index url """
         self.assertEqual(reverse('manager:index'), '/manager/')
 
     def test_categories_url(self):
+        """ Test categories url """
         self.assertEqual(reverse('manager:categories'), '/manager/categories/')
 
     def test_category_create_url(self):
+        """ Test categories-create url """
         self.assertEqual(
             reverse('manager:category-create'), '/manager/category-create/'
         )
 
     def test_category_edit_url(self):
+        """ Test categories-edit url """
         self.assertEqual(
             reverse('manager:category-edit', args=(1,)),
             '/manager/category-edit/1/'
         )
 
     def test_category_delete_url(self):
+        """ Test categories-delete url """
         self.assertEqual(
             reverse('manager:category-delete', args=(1,)),
             '/manager/category-delete/1/'
         )
 
     def test_products_url(self):
+        """ Test products url """
         self.assertEqual(reverse('manager:products'), '/manager/products/')
 
     def test_product_create_url(self):
+        """ Test product-create url """
         self.assertEqual(
-            reverse('manager:category-create'), '/manager/category-create/'
+            reverse('manager:product-create'), '/manager/product-create/'
         )
 
     def test_product_edit_url(self):
+        """ Test product-edit url """
         self.assertEqual(
             reverse('manager:product-edit', args=(1,)),
             '/manager/product-edit/1/'
         )
 
     def test_product_delete_url(self):
+        """ Test product-delete url """
         self.assertEqual(
             reverse('manager:product-delete', args=(1,)),
             '/manager/product-delete/1/'
