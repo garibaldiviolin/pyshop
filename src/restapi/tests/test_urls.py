e7c5b7afe0f5fd5b7c8c8e5dfc5d2e24e1166470
garibaldiviolin <garibaldiviolin@gmail.com>
2018-10-06 13:32:44 -0300

    Update restapi/tests/test_urls.py docstrings

--- src/restapi/tests/test_urls.py
+++ src/restapi/tests/test_urls.py
@@ -8,15 +8,15 @@ class RESTAPIURLsTest(TestCase):
     """ Test case for the restapi app urls """
 
     def test_categories_url(self):
-        """ Test restapi:categories url """
+        """ Test categories url """
         self.assertEqual(reverse('restapi:categories'), '/api/v1/categories/')
 
     def test_products_url(self):
-        """ Test restapi:products url """
+        """ Test products url """
         self.assertEqual(reverse('restapi:products'), '/api/v1/products/')
 
     def test_payment_methods_url(self):
-        """ Test restapi:payment-methods url """
+        """ Test payment-methods url """
         self.assertEqual(
             reverse('restapi:payment-methods'),
             '/api/v1/payment-methods/'
