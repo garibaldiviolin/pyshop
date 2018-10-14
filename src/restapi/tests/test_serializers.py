c47280fa78fa48da2d7692a99c4a80c2885fbfc9
garibaldiviolin <garibaldiviolin@gmail.com>
2018-10-13 23:39:17 -0300

    Add PurchaseItemSerializer tests to restapi/tests/test_serializers.py

--- src/restapi/tests/test_serializers.py
+++ src/restapi/tests/test_serializers.py
@@ -7,12 +7,19 @@ from django.test import TestCase
 from rest_framework import serializers
 
 from pyshop.settings import BASE_DIR
-from website.models import Category, Product, PaymentMethod, PurchaseOrder
+from website.models import (
+    Category,
+    Product,
+    PaymentMethod,
+    PurchaseOrder,
+    PurchaseItem
+)
 from ..serializers import (
     CategorySerializer,
     ProductSerializer,
     PaymentMethodSerializer,
     PurchaseOrderSerializer,
+    PurchaseItemSerializer,
 )
 
 
@@ -361,3 +368,176 @@ class PurchaseOrderSerializerTest(TestCase):
             'null',
             str(serializer.errors.values())
         )
+
+
+class PurchaseItemSerializerTest(TestCase):
+    """ Test case for the PurchaseItem model serializer """
+
+    def setUp(self):
+        self.image_path = os.path.join(
+            BASE_DIR, 'website/fixtures/', 'sample_image.jpg'
+        )
+        self.image = open(self.image_path, 'rb')
+
+        self.category = Category.objects.create(description='Furnitures')
+
+        self.product = Product.objects.create(
+            barcode='789789789',
+            title='Mattress',
+            description='Sleep Mattress',
+            image=self.image,
+            price=123.000,
+            category=self.category
+        )
+
+        self.user = User.objects.get_or_create(username='testuser')[0]
+        self.purchase_order = PurchaseOrder.objects.create(
+            timestamp='2018-10-09T01:15:00Z', user=self.user, cart=False
+        )
+
+        self.purchase_item = PurchaseItem(
+            id=1,
+            title=self.product.title,
+            description=self.product.description,
+            image=self.product.image,
+            price=self.product.price,
+            barcode=self.product.barcode,
+            quantity=1.000,
+            total_price=self.product.price,
+            category=self.product.category,
+            purchase_order=self.purchase_order
+        )
+
+        self.json = {
+            'id': 1,
+            'title': self.product.title,
+            'description': self.product.description,
+            'image': self.product.image,
+            'price': self.product.price,
+            'barcode': self.product.barcode,
+            'quantity': '1.000',
+            'total_price': self.product.price,
+            'category': self.product.category.id,
+            'purchase_order': 1
+        }
+
+        self.serializer = PurchaseItemSerializer(
+            instance=self.purchase_item
+        )
+
+    def test_barcode_content(self):
+        """ Test barcode field value """
+
+        self.assertEqual(
+            self.purchase_item.barcode,
+            self.serializer.data['barcode']
+        )
+
+    def test_title_content(self):
+        """ Test title field value """
+
+        self.assertEqual(
+            self.purchase_item.title,
+            self.serializer.data['title']
+        )
+
+    def test_description_content(self):
+        """ Test description field value """
+
+        self.assertEqual(
+            self.purchase_item.description,
+            self.serializer.data['description']
+        )
+
+    def test_price_content(self):
+        """ Test price field value """
+
+        self.assertEqual(
+            self.purchase_item.price,
+            float(self.serializer.data['price'])
+        )
+
+    def test_category_content(self):
+        """ Test category id field value """
+
+        self.assertEqual(
+            self.purchase_item.category.id,
+            self.serializer.data['category']
+        )
+
+    def test_blank_barcode(self):
+        """ Test using blank barcode value """
+
+        data = self.json
+        data.update({'barcode': ''})
+        exception_serializer = PurchaseItemSerializer(data=data)
+
+        with self.assertRaises(serializers.ValidationError):
+            exception_serializer.is_valid(raise_exception=True)
+        self.assertIn('barcode', exception_serializer.errors.keys())
+        self.assertIn('blank', str(exception_serializer.errors.values()))
+
+    def test_blank_title(self):
+        """ Test using blank title value """
+
+        data = self.json
+        data.update({'title': ''})
+        exception_serializer = PurchaseItemSerializer(data=data)
+
+        with self.assertRaises(serializers.ValidationError):
+            exception_serializer.is_valid(raise_exception=True)
+        self.assertIn('title', exception_serializer.errors.keys())
+        self.assertIn('blank', str(exception_serializer.errors.values()))
+
+    def test_blank_description(self):
+        """ Test using blank description value """
+
+        data = self.json
+        data.update({'description': ''})
+        exception_serializer = PurchaseItemSerializer(data=data)
+
+        with self.assertRaises(serializers.ValidationError):
+            exception_serializer.is_valid(raise_exception=True)
+        self.assertIn('description', exception_serializer.errors.keys())
+        self.assertIn('blank', str(exception_serializer.errors.values()))
+
+    def test_blank_image(self):
+        """ Test using blank image value """
+
+        data = self.json
+        data.update({'image': ''})
+        exception_serializer = PurchaseItemSerializer(data=data)
+
+        with self.assertRaises(serializers.ValidationError):
+            exception_serializer.is_valid(raise_exception=True)
+        self.assertIn('image', exception_serializer.errors.keys())
+        self.assertIn(
+            'The submitted data was not a file',
+            str(exception_serializer.errors.values())
+        )
+
+    def test_blank_price(self):
+        """ Test using blank price value """
+
+        data = self.json
+        data.update({'price': ''})
+        exception_serializer = PurchaseItemSerializer(data=data)
+
+        with self.assertRaises(serializers.ValidationError):
+            exception_serializer.is_valid(raise_exception=True)
+        self.assertIn('price', exception_serializer.errors.keys())
+        self.assertIn(
+            'valid number', str(exception_serializer.errors.values())
+        )
+
+    def test_null_category(self):
+        """ Test using null category id value """
+
+        data = self.json
+        data.update({'category': ''})
+        exception_serializer = ProductSerializer(data=data)
+
+        with self.assertRaises(serializers.ValidationError):
+            exception_serializer.is_valid(raise_exception=True)
+        self.assertIn('category', exception_serializer.errors.keys())
+        self.assertIn('null', str(exception_serializer.errors.values()))
